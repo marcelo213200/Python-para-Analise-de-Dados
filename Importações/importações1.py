e804a0086caf15c4d552f8f6c7c41c2e29ec6759
Marcelo Passos <72776486+marcelo213200@users.noreply.github.com>
2021-03-07 21:57:46 -0300

    Delete Importações directory

--- Importações/importações1.py
+++
@@ -1,8 +0,0 @@
-PI = 3.14159
-
-def f(x):
-    return x + 2
-
-def g(a,b):
-    return a + b
-
--- Importações/importações2.py
+++
@@ -1,9 +0,0 @@
-import importações1
-
-result = importações1.f(5)
-pi = importações1.PI
-
-#OU
-
-#from importações1 import f,g,PI
-#result = g(5,PI)
