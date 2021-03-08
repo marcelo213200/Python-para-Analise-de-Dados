de6c8eed6c9e0a3d700b0f0abfeed3fc97a8ffda
Marcelo Passos <72776486+marcelo213200@users.noreply.github.com>
2021-03-07 21:01:35 -0300

    Delete Data Science directory

--- Data Science/FunçõesEmbutidas/enumerate.py
+++
@@ -1,6 +0,0 @@
-# Controle de índice 
-
-jogo = [[0,0,0],[0,0,0],[0,0,0]]
-print("   a  b  c")
-for conta, linha in enumerate(jogo):
-    print(conta,linha)
--- Data Science/Laços/exp_ternarias.py
+++
@@ -1,6 +0,0 @@
-#Expressões Ternárias
-#permite combinar um bloco if-else que gera um valor em uma única linha ou expressão
-
-x = 5
-
-'Positivo' if x > 0 else 'Negative'
